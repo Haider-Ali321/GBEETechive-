2c5f33b6a6d292a3146ccf8f7c2a171f9f49bce6
Haider-Ali321 <haideralikhanhydree@gmail.com>
2023-12-15 23:46:54 +0500

    8th commit

---
+++ Script/script.js
@@ -0,0 +1,17 @@
+$('.testimonials-container').owlCarousel({
+  loop:true,
+  margin:30,
+  nav:true,
+  dots: false,
+  responsive:{
+      0:{
+          items:1
+      },
+    //   600:{
+    //       items:2
+    //   },
+        1000:{
+        items:2
+    }
+  }
+})
